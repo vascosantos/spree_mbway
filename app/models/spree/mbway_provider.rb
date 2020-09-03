563a684bafcfc63f24378d79b044e0bffe097b27
Vasco Santos <vasco.santos@inlogic.pt>
2020-09-03 16:54:21 +0100

    add api urls

--- app/models/spree/mbway_provider.rb
+++ app/models/spree/mbway_provider.rb
@@ -1,9 +1,12 @@
 module Spree
   class MBWayProvider < ActiveRecord::Base
   	has_many :payments
-
     validates :name, :key, presence: true
     
     scope :active, -> { where(active: true) }
+
+    API_BASE_URL = "https://www.ifthenpay.com/mbwayws/IfthenPayMBW.asmx"
+    API_SET_URL = API_BASE_URL + "/SetPedidoJSON"
+    API_STATE_URL = API_BASE_URL + "/EstadoPedidosJSON"
   end
 end
